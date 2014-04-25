7843471c58dc05eddb450dadd97cd36e4480f54e
Alberto96 <albertoguanti@gmail.com>
2014-04-25 12:46:47 +0200

    repo: cleanup

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 1 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Thu Apr 24 21:58:53 CEST 2014"
-#define LINUX_COMPILE_TIME "21:58:53"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "alberto96-System-Product-Name"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
